aa3b870adc0332db8e1554e8e5f5b26181a802cd
cpccharry <harry.subramaniam@cpcc.edu>
2021-10-02 12:06:10 -0400

    Initial release.

---
+++ src/main/java/edu/cpcc/labs/SpringbootGitWozuLaboneApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitWozuLaboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitWozuLaboneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cpcc/labs/SpringbootGitWozuLaboneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitWozuLaboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
